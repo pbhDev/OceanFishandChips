804a229892b0b32499b5a5c9ed991855c220296b
pbhDev <pat.helles@gmail.com>
2018-04-28 14:22:53 -0700

    Initial commit

---
+++ app/src/main/java/com/example/android/oceanfishandchips/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.oceanfishandchips;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+// images source http://oceanfishandchips.net/
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
